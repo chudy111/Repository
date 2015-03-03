8eb550d92ea8e0967d2f3785f2b69801955b1ef9
chudy111 <chudy111@gmail.com>
2015-03-03 21:43:46 +0100

    updated AlgorytmA

--- C++/AlgorytmA/AlgorytmA/TableAStar.cpp
+++ C++/AlgorytmA/AlgorytmA/TableAStar.cpp
@@ -4,15 +4,20 @@
 
 CTableAStar::CTableAStar()
 {
+	m_oOL = new vector<CBlock*>();
+	m_oCL = new vector<CBlock*>();
 }
 
 
 CTableAStar::~CTableAStar()
 {
+	
 }
 
 void CTableAStar::setTable(int a_iX, int a_iY)
 {
+	m_iX = a_iX;
+	m_iY = a_iY;
 	m_poTable = new CBlock*[a_iX]();
 	for (int i = 0; i < a_iX; i++)
 	{
@@ -33,4 +38,71 @@ void CTableAStar::setEnd(int a_iX, int a_iY)
 void CTableAStar::setWall(int a_iX, int a_iY)
 {
 	m_poTable[a_iX][a_iY].setCanMove(false);
+}
+
+void CTableAStar::addOL(int a_iX, int a_iY)
+{
+	m_oOL->push_back(&(m_poTable[a_iX][a_iY]));
+}
+
+void CTableAStar::removeOL(int a_iX, int a_iY)
+{
+	for (int i = 0; i < m_oOL->size(); i++)
+	{
+		if (m_oOL->at(i) == &(m_poTable[a_iX][a_iY]))
+		{
+			m_oOL->erase(m_oOL->begin() + i);
+			break;
+		}
+	}
+}
+
+void CTableAStar::addCL(int a_iX, int a_iY)
+{
+	m_oCL->push_back(&(m_poTable[a_iX][a_iY]));
+}
+
+void CTableAStar::removeCL(int a_iX, int a_iY)
+{
+	for (int i = 0; i < m_oCL->size(); i++)
+	{
+		if (m_oCL->at(i) == &(m_poTable[a_iX][a_iY]))
+		{
+			m_oCL->erase(m_oCL->begin() + i);
+			break;
+		}
+	}
+}
+
+CBlock* CTableAStar::findMinFFromOL()
+{
+	CBlock* oMinBlock = m_oOL->at(0);
+	for (int i = 0; i < m_oOL->size(); i++)
+	{
+		if (oMinBlock->getF() >= m_oOL->at(i)->getF())
+		{
+			oMinBlock = m_oOL->at(i);
+		}
+	}
+	return oMinBlock;
+}
+
+
+string CTableAStar::solve()
+{
+	CBlock* oTempBlock;
+	m_oOL->push_back(m_poStartPoint);
+	while (!m_oOL->empty)
+	{
+		oTempBlock = findMinFFromOL();
+		removeOL(oTempBlock->getX(), oTempBlock->getY());
+		addCL(oTempBlock->getX(), oTempBlock->getY());
+		if (oTempBlock == m_poEndPoint)
+		{
+			break;
+		}
+	}
+
+
+	return m_strPath;
 }
--- C++/AlgorytmA/AlgorytmA/TableAStar.h
+++ C++/AlgorytmA/AlgorytmA/TableAStar.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <string>
+#include <iostream>
 #include "Block.h"
 #include <vector>
 using namespace std;
@@ -11,12 +13,20 @@ public:
 	void setWall(int, int);
 	void setStart(int, int);
 	void setEnd(int, int);
+	void addOL(int, int);
+	void removeOL(int, int);
+	void addCL(int, int);
+	void removeCL(int, int);
+	string solve();
+	CBlock* findMinFFromOL();
 private:
 	CBlock** m_poTable;
 	CBlock* m_poStartPoint;
 	CBlock* m_poEndPoint;
-	vector <CBlock*> m_oOL;
-	vector <CBlock*> m_oCL
-
+	vector <CBlock*>* m_oOL;
+	vector <CBlock*>* m_oCL;
+	int m_iX;
+	int m_iY;
+	string m_strPath;
 };
 
